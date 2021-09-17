7c030534d66c402f08e97352f3a8fe74de064181
kiota-rita <rita-kiota@hotmail.com>
2021-09-17 15:34:57 -0300

    add bulma css

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
 	   end
    
 	   def update
-   
+		@post = Post.find(params[:id])
 		   if @post.update(post_params)
 			   redirect_to @post
 		   else
